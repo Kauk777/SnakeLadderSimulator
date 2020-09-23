9bb5ff1dbb17b448d1a13af58687c7718d0f5f19
Kauk777 <curokky777@gmail.com>
2020-09-23 21:08:31 +0530

    [KAUSHAL]Added: Snake Ladder UC2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,7 @@ public class SnakeLadder {
  public static final int START_POSITION=0;
  public static void main(String[] args) {
    System.out.println("Welcome to Snake & Ladder Simulator");
-   
+   int dieRoll=(int)(Math.random()*6) + 1;
+   System.out.println("Die Roll Number: "+dieRoll);
  }
 }
