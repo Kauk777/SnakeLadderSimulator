36796c22fc38157b21757c30c51b6eaabf03f1e1
Kauk777 <curokky777@gmail.com>
2020-09-23 21:15:26 +0530

    [KAUSHAL]Added: Snake Ladder UC3

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,18 @@
 public class SnakeLadder {
  public static final int START_POSITION=0;
+ public static final int NO_PLAY=1;
+ public static final int LADDER_POS=2;
+ public static final int SNAKE_POS=3;
  public static void main(String[] args) {
-   System.out.println("Welcome to Snake & Ladder Simulator");
+   int pos=0;
    int dieRoll=(int)(Math.random()*6) + 1;
    System.out.println("Die Roll Number: "+dieRoll);
+   int playerOption= (int)(Math.random()*3) + 1;
+   System.out.println("Option: "+playerOption);
+   if(playerOption == LADDER_POS)
+     pos+=dieRoll;
+   if(playerOption == SNAKE_POS)
+     pos-=dieRoll;
+   System.out.println("Position: "+pos);
  }
 }
