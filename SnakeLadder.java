f7500454a4a217346f56504e686c7bc7cc1c2715
Kauk777 <curokky777@gmail.com>
2020-09-23 23:03:43 +0530

    [KAUSHAL]Added: Snake Ladder UC7

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,27 +5,52 @@ public class SnakeLadder {
  public static final int LADDER_POS=2;
  public static final int SNAKE_POS=3;
  public static void main(String[] args) {
-   int pos=0, flag=0, count=0;
+   int pos1=0, pos2=0, flag=0, count=0;
    while(flag==0) {
-   int dieRoll=(int)(Math.random()*6) + 1;
-   System.out.println("Die Roll Number: "+dieRoll);
-   int playerOption= (int)(Math.random()*3) + 1;
-   System.out.println("Option: "+playerOption);
-   if(playerOption == LADDER_POS)
-     pos+=dieRoll;
-   if(playerOption == SNAKE_POS)
-     pos-=dieRoll;
-   if(pos<0)
-     pos=0;
-   if(pos>100)
-     pos-=dieRoll;
-   System.out.println("Position: "+pos);
-   if(pos==WIN_POSITION)
+   int dieRollP1=(int)(Math.random()*6) + 1;
+   System.out.println("Player1 Die Roll Number: "+dieRollP1);
+   int player1Option= (int)(Math.random()*3) + 1;
+   System.out.println("Player1 Option: "+player1Option);
+   if(player1Option == LADDER_POS)
+     pos1+=dieRollP1;
+   if(player1Option == SNAKE_POS)
+     pos1-=dieRollP1;
+   if(pos1<0)
+     pos1=0;
+   if(pos1>100)
+     pos1-=dieRollP1;
+   System.out.println("Player1 Position: "+pos1);
+   if(pos1==WIN_POSITION)
     {
        flag=1;
-       System.out.println("PLAYER WON");
+       System.out.println("PLAYER-1 WON");
        break;
     }
+    if(player1Option == LADDER_POS)
+      continue;
+    else {
+      int dieRollP2=(int)(Math.random()*6) + 1;
+      System.out.println("Player2 Die Roll Number: "+dieRollP2);
+      int player2Option= (int)(Math.random()*3) + 1;
+      System.out.println("Player2 Option: "+player2Option);
+      if(player2Option == LADDER_POS)
+        pos2+=dieRollP2;
+      if(player2Option == SNAKE_POS)
+        pos2-=dieRollP2;
+      if(pos2<0)
+        pos2=0;
+      if(pos2>100)
+        pos2-=dieRollP2;
+      System.out.println("Player2 Position: "+pos2);
+      if(pos2==WIN_POSITION)
+      {
+         flag=1;
+         System.out.println("PLAYER-2 WON");
+         break;
+      }
+      if(player2Option == LADDER_POS)
+      continue;
+   }
     count++;
   }
    System.out.println("Player Won after: "+count+" Count");
